4a5ada4eb8587d71ac6af4dc225a86d40ae4cb7e
NahumFG <ngutierrez2507@gmail.com>
2024-05-16 21:00:53 -0500

    simple-jwt

--- api_login_project/settings.py
+++ api_login_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import datetime
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -151,3 +152,11 @@ SWAGGER_SETTINGS = {
 
 # Configuración extendida de usuarios
 AUTH_USER_MODEL = "users.User"
+
+# Simple JWT
+# https://django-rest-framework-simplejwt.readthedocs.io/en/latest/getting_started.html
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
+}
