d943d723bdc524b41a15f677c67430717a7a98a0
NahumFG <ngutierrez2507@gmail.com>
2024-05-16 19:28:48 -0500

    instalación de rest_framework,  coreheaders y swagger

--- api_login_project/urls.py
+++ api_login_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for api_login_project project.
+URL configuration for api_gestorpagos project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -14,9 +14,37 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 
+from django.contrib import admin
+from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version="v1",
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # Rutas de la documentación de la API
+    path(
+        "docs/v1/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path(
+        "redocs/v1/",
+        schema_view.with_ui("redoc", cache_timeout=0),
+        name="schema-redoc",
+    ),
+    # Rutas de la API
+    path("admin/", admin.site.urls),
 ]
